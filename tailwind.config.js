7d93558f03067c990e2fe0c47d9e743b52759092
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-03-21 15:58:48 +0200

    feat - add height and color theme in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4F7DF3",
+      },
+      height: {
+        40: "40vh",
+      },
+    },
   },
   plugins: [],
 };
