7e0b69dc6de076a90834c2199607ff7ef9d0d876
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-03-21 15:59:12 +0200

    fix

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="px-20">
         <div class="flex justify-center flex-col">
-            <h1 class="text-center">We are launching soon!</h1>
-            <h4 class="text-center">Subscribed and get notified</h4>
+            <div class="flex justify-center	flex-row">
+                <h1 class="text-center text-5xl font-thin">
+                    We are launching
+                </h1>
+                <h1 class="text-center text-5xl ml-2">soon!</h1>
+            </div>
+            <h4 class="text-center text-2xl mt-3 font-normal">
+                Subscribed and get notified
+            </h4>
         </div>
         <div class="flex justify-center py-12">
             <form class="flex flex-row">
-                <input label="email-address" placeholder="Your email address" />
-                <button class="bg-cyan-500 rounded-md text-white w-full p-2">Notify Me</button>
+                <input label="email-address" placeholder="Your email address..."
+                    class="mx-2.5 p-3 border border-primary rounded-full h-14 w-96" />
+                <button class="mx-2.5 bg-primary rounded-full text-white p-2.5 w-40">Notify Me</button>
             </form>
         </div>
-        <div class="px-52">
-            <img src="../assets/images/illustration-dashboard.png" />
+        <div class="flex justify-center px-52">
+            <img src="../assets/images/illustration-dashboard.png" class="h-40" />
         </div>
     </div>
 </template>
